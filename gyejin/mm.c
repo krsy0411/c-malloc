42b0cd240715944e192dadd80d702e1121f5a6f1
root <whrpwls1234@hanmail.net>
2025-08-23 23:58:26 +0900

    feat: Implement memory allocator functions(define macros, mm_init, expand_heap)

--- gyejin/mm.c
+++ gyejin/mm.c
@@ -42,12 +42,41 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+static char *heap_listp = NULL;     //힙 공간 생성할 heap_listp생성
+
 /*
  * mm_init - initialize the malloc package.
  */
-int mm_init(void)
-{
+int mm_init(void){
+    heap_listp = mem_sbrk(4*WSIZE);     //비어있는 힙 생성 (힙 영역 brk기준으로 16 늘림)
+    if (heap_listp == (void *) -1){     //빈 힙이면 생성 안된 것
+        return -1;
+    }
+
+    PUT(heap_listp, 0);     //패딩 할당 - 맨 앞 4바이트 공간에 0을 넣음, 8바이트 정렬이라서!
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    //프롤로그 헤더 : 힙 맨앞에 세워둠, 맨앞을 넘어가서 PREV_BLKP매크로 만나면 예외처리, 사이즈랑 할당 여부 1 지정
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    //프롤로그 푸터 : 푸터는 헤더 4바이트 뒤 공간
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        //에필로그 헤더 : 힙의 맨끝을 표시하는 경계선, 끝을 넘어가서 NEXT_BLKP매크로 만나면 예외처리
+    heap_listp += (2*WSIZE);        //포인터 위치 8바이트 뒤로 옮김, 시작점 8 시작
+
     return 0;
+}
+
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;   //사이즈를 짝수 * 4 는 항상 8의 배수가 되므로(홀수 * 4는 8의 배수 안됨)
+    if ((long)(bp = mem_sbrk(size)) == -1){     //size만큼 bp를 늘림, 안되면 NULL
+        return NULL;
+    }
+
+    PUT(HDRP(bp), PACK(size, 0));           //가용 블럭 헤더(가용이니까 0)
+    PUT(FTRP(bp), PACK(size, 0));           //가용 블럭 푸터
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));   //새로운 에필로그 헤더(할당 1)
+
+    //return coalesce(bp);
+    return bp;
 }
 
 /*
